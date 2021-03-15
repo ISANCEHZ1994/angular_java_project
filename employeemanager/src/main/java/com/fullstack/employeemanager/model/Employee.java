134046b0d4d591e4da4b665ed2f4221d63c8a70d
Israel Sanchez <Israel.pro1994@gmail.com>
2021-03-15 14:50:52 -0500

    added Entity

--- employeemanager/src/main/java/com/fullstack/employeemanager/model/Employee.java
+++ employeemanager/src/main/java/com/fullstack/employeemanager/model/Employee.java
@@ -1,11 +1,14 @@
 package com.fullstack.employeemanager.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity // will make sure that this class is mapped to any database that we have configured on class path
 public class Employee implements Serializable {
     // serializable helps transform class to send different types of string
     // will be sending some information to database as well as to frontend
 
+    @Id // we want id to be the primary key in the database
     private Long id;
     private String name;
     private String email;
