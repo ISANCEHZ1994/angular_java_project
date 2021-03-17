82b20b0be06e36e3dff0a868e5e66524dcded85c
Israel Sanchez <Israel.pro1994@gmail.com>
2021-03-16 21:14:41 -0500

    working on api then controllers

--- employeemanager/src/main/java/com/fullstack/employeemanager/EmployeeResource.java
+++ employeemanager/src/main/java/com/fullstack/employeemanager/EmployeeResource.java
@@ -1,5 +1,26 @@
 package com.fullstack.employeemanager;
 
+import com.fullstack.employeemanager.model.Employee;
+import com.fullstack.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeResource {
-    
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 }
