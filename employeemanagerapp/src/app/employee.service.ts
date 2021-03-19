5e9e12b09af88d662b39d8284d9b0ae548601e08
Israel Sanchez <Israel.pro1994@gmail.com>
2021-03-19 15:52:48 -0500

    controllers created for frontend - working on html file (displaying employees)

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class EmployeeService{
-    private apiServerURL = '';
+    private apiServerURL = environment.apiBasedUrl;
 
     constructor(private http: HttpClient){
 
     }
 
     public getEmployees(): Observable<Employee[]> {
-        return this.http.get<any>(`${this.apiServerURL}/employee/all`);
+        return this.http.get<Employee[]>(`${this.apiServerURL}/employee/all`);
+    }
+
+    public addEmployees(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerURL}/employee/add`, employee);
+    }
+
+    public updateEmployees(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerURL}/employee/update`, employee);
+    }
+
+    public deleteEmployees(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerURL}/employee/delete/${employeeId}`);
     }
 }
 
