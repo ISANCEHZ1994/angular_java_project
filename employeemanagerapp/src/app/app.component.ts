030cf62a05f767a931f9aef4aa522b323179467e
Israel Sanchez <Israel.pro1994@gmail.com>
2021-03-19 18:58:56 -0500

    adding functionality to frontend work

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -29,6 +29,19 @@ export class AppComponent implements OnInit {
      }
     );
   }
+
+  public onOpenModal(employee: Employee, mode: string): void{
+    const button = document.createElement('button');
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target', 'addEmployeeModal');
+    }
+
+  }
    
   }
 
